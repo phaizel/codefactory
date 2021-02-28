04b5eddd0f59deba9156a8ed3cb09e267c7a4a8c
Elton Muteswa <phaizel@gmail.com>
2021-02-28 15:47:06 +0100

    transaction details

--- src/main/kotlin/code/challenge/bank/Transactions.kt
+++ src/main/kotlin/code/challenge/bank/Transactions.kt
@@ -1,9 +1,23 @@
 package code.challenge.bank
 
 import java.math.BigDecimal
+import java.time.OffsetDateTime
 
 sealed class TransactionRequest {
     data class Debit(val account: BankAccount, val amount: BigDecimal) : TransactionRequest()
     data class Credit(val account: BankAccount, val amount: BigDecimal) : TransactionRequest()
     data class Transfer(val from: BankAccount, val to: BankAccount, val amount: BigDecimal) : TransactionRequest()
 }
+
+sealed class TransactionStatus {
+    object Approved : TransactionStatus()
+    class Declined(vararg val reasons: String) : TransactionStatus()
+    object Executed : TransactionStatus()
+}
+
+data class Transaction (
+    val request: TransactionRequest,
+    val status: TransactionStatus,
+    val result: BankAccount,
+    val dateTimeStamp: OffsetDateTime = OffsetDateTime.now()
+)
