7a098573c44aef481da9db04d948b8105afa25fc
Elton Muteswa <phaizel@gmail.com>
2021-02-28 15:30:45 +0100

    savings account linked with checking account only via the use of account attributes

--- src/main/kotlin/code/challenge/bank/Accounts.kt
+++ src/main/kotlin/code/challenge/bank/Accounts.kt
@@ -2,10 +2,19 @@ package code.challenge.bank
 
 import java.math.BigDecimal
 
+sealed class AccountAttribute {
+    data class ReferenceAccount(val account: BankAccount) : AccountAttribute()
+}
+
 sealed class BankAccount {
     abstract val iban: String
     open val balance: BigDecimal = BigDecimal.ZERO
+    open val attributes: List<AccountAttribute> = emptyList()
+
     data class Checking(override val iban: String, override val balance: BigDecimal) : BankAccount()
-    data class Savings(override val iban: String, override val balance: BigDecimal) : BankAccount()
+    data class Savings(override val iban: String, private val referenceAccount: Checking, override val balance: BigDecimal) : BankAccount() {
+        override val attributes: List<AccountAttribute>
+            get() = listOf(AccountAttribute.ReferenceAccount(referenceAccount))
+    }
     data class PrivateLoan(override val iban: String, override val balance: BigDecimal) : BankAccount()
 }
